8b32edf8198ce9e3dd1cd26cc8cdb3ad6e303b4e
ongaeshi <ongaeshi0621@gmail.com>
2015-12-15 16:33:55 +0900

    Add 天気.rb

---
+++ ja/天気.rb
@@ -0,0 +1,75 @@
+# # 天気.rb
+# ## 概要
+# Weather Hacks から今日の天気情報を取得して表示
+# ## 使い方
+# 1. CITYに天気を表示したい地域コードの配列を入力
+# 2. 地域コードは那覇だと http://weather.livedoor.com/area/forecast/471010 の最後の数字です
+# 3. 実行結果を毎日テキストに記録したりすると楽しいです
+
+CITY = [016010, 130010, 270000, 471010] # 地域コードを入れます
+
+def convert
+  t = Time.now
+  date = t.datestr
+  
+  "#{date} #{t.hour}:#{t.min} の天気\n\n" + 
+  CITY.map { |e|
+    LivedoorWeather.new(e).show(date)
+  }.join("---\n")
+end
+
+class LivedoorWeather
+  attr_reader :data
+  
+  def initialize(city)
+    @city = city
+    @data = Browser.json("http://weather.livedoor.com/forecast/webservice/json/v1?city=#{city}")
+  end
+  
+  def show(today)
+  <<EOS 
+#{day_str(today)}の#{area}は#{telop(0)}(気温は#{temperature(0, "min")}から#{temperature(0, "max")}度)。
+明日は#{telop(1)}(気温は#{temperature(1, "min")}から#{temperature(1, "max")}度)の予定。
+#{url}
+EOS
+  end
+  
+  def day_str(today)
+    if today == date(0)
+      "今日" 
+    else
+      date(0)
+    end
+  end
+
+  def telop(idx)
+    @data["forecasts"][idx]["telop"]
+  end
+  
+  def temperature(idx, kind)
+     t = @data["forecasts"][idx]["temperature"][kind]
+     t ? t["celsius"]  : "?"
+  end
+   
+  def date(idx)
+     @data["forecasts"][idx]["date"]
+  end
+
+  def area
+    @data["location"]["city"]
+  end
+  
+  def description
+    @data["description"]["text"]
+  end
+  
+  def url
+    "http://weather.livedoor.com/lite/area/forecast/#{@city}"
+  end
+end
+
+class Time
+  def datestr
+    sprintf("%04d-%02d-%02d", year, month, day)
+  end
+end
