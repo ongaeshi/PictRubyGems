ef0be9262414854d489e9f39afd88302e5f67bf6
ongaeshi <ongaeshi0621@gmail.com>
2016-01-06 12:42:05 +0900

    Add ja/突然の死

---
+++ ja/突然の死.rb
@@ -0,0 +1,27 @@
+# Please return the text or image in the "def convert"
+
+def convert
+  word = Popup.input("何？")
+  
+  <<EOS
+#{header(word)}
+＞#{word}＜
+#{footer(word)}
+EOS
+end
+
+def header(word)
+  f(word, "人" , "＿")
+end
+
+def footer(word)
+  f(word, "Ｙ", "￣", "^", true)
+end
+
+def f(word, a, b, c = " ", swap = false)
+  w = word.size / 2
+  l = a * (w + w % 2)
+  r = a * w
+  l, r = r, l if swap
+  b + l + c + r + b
+end
